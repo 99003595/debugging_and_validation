165011f995382539bff6ab7b5967d10461366057
99003595 <78578958+99003595@users.noreply.github.com>
2021-03-23 09:47:40 +0530

    Add files via upload

---
+++ step10/userspace/ioctltest.c
@@ -0,0 +1,72 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+
+#define IOC_MAGIC       'p'
+#define MY_IOCTL_LEN    _IO(IOC_MAGIC,1)
+#define MY_IOCTL_AVAIL  _IO(IOC_MAGIC,2)
+#define MY_IOCTL_RESET  _IO(IOC_MAGIC,3)
+
+
+
+int main()
+{
+ int ret,fd,nbytes ;
+        fd=open("/dev/psample 0", O_RDWR);
+
+        if(fd<0) {
+
+          perror("open");
+
+          exit(1);
+
+       }
+
+       char str[]="Hello Kernel";
+
+        int len=strlen(str);
+
+        nbytes=write(fd,str,len);
+
+        if(nbytes<0) {
+
+          perror("write");
+
+        }
+
+ 
+
+        char buf[64];
+
+        int maxlen=64;
+
+        nbytes=read(fd,buf,maxlen);
+
+        
+
+
+        ret=ioctl(fd, MY_IOCTL_LEN);
+        if(ret<0)
+          {
+            perror("ioctl");
+            exit(3);
+          }
+        ret=ioctl(fd, MY_IOCTL_AVAIL);
+        if(ret<0)
+          {
+            perror("ioctl");
+            exit(3);
+          }
+        ret=ioctl(fd, MY_IOCTL_RESET);
+        if(ret<0) 
+          {
+            perror("ioctl");
+            exit(3);
+          }
+
+
+        close(fd);
+}
